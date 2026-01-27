5ed2856e16c8b533375d0f6411b800de54a807b1
dmitry992 <dima.boch4@gmail.com>
2026-01-27 17:28:23 +0500

    day 12, task 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
+        $middleware->alias([
+            'access' => \App\Http\Middleware\Access::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
     })->create();
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,12 @@ Route::middleware('auth')->group(function () {
 /**
  * Проекты
  */
-Route::get('projects', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('projects/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::post('projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
-Route::get('projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::put('projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+Route::middleware('access')->group(function () {
+    Route::get('projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::get('projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+});
